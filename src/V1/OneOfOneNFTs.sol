fd1525a33c89237fce0bc8e52e6521e65bd87139
AndrewSing1 <AndrewSingaley@outlook.com>
2025-10-09 09:31:40 -0700

    feat: forge fmt

--- src/V1/OneOfOneNFTs.sol
+++ src/V1/OneOfOneNFTs.sol
@@ -14,20 +14,16 @@ import "../interfaces/IMetadataRenderer.sol";
  * @dev NFT contract that changes metadata based on external data sources
  */
 contract OneOfOneNFTs is ERC721, ERC721URIStorage, Ownable {
-
-        // Core interfaces
+    // Core interfaces
     IDataOracle public weatherOracle;
     IDataOracle public timeOracle;
     IMetadataRenderer public metadataRenderer;
 
-    constructor(
-        address _weatherOracle,
-        address _timeOracle,
-        address _metadataRenderer
-    ) ERC721("Dynamic Weather NFT", "DYNFT") {
+    constructor(address _weatherOracle, address _timeOracle, address _metadataRenderer)
+        ERC721("Dynamic Weather NFT", "DYNFT")
+    {
         weatherOracle = IDataOracle(_weatherOracle);
         timeOracle = IDataOracle(_timeOracle);
         metadataRenderer = IMetadataRenderer(_metadataRenderer);
     }
 }
-    
