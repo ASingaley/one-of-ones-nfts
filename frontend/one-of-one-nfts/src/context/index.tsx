f75074084b17b045e17e8caddb9c0d5624b546a9
AndrewSing1 <AndrewSingaley@outlook.com>
2025-10-13 17:38:57 -0700

    feat: Refactor Code

--- frontend/one-of-one-nfts/src/context/index.tsx
+++ frontend/one-of-one-nfts/src/context/index.tsx
@@ -11,8 +11,8 @@ const queryClient = new QueryClient()
 
 // Set up metadata
 const metadata = {
-  name: 'next-reown-appkit',
-  description: 'next-reown-appkit',
+  name: 'one-of-one-nfts',
+  description: 'one of one nft mints',
   url: 'https://github.com/0xonerb/next-reown-appkit-ssr', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
