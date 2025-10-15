ea7be00ea16f78ac6def40444762613112761795
AndrewSing1 <AndrewSingaley@outlook.com>
2025-10-15 15:19:52 -0700

    feat: Add Necessary MetadataRenderer Contract Imports

--- src/V1/MetadataRenderer.sol
+++ src/V1/MetadataRenderer.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MetadataRenderer {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "../interfaces/IMetadataRenderer.sol";
+
+contract MetadataRenderer  {}
