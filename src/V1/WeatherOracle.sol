d0c4262a4f2077dbf0e9d6959fd0eedd9c638a86
ASingaley <AndrewSingaley@outlook.com>
2025-11-30 10:49:54 -0800

    feat: forge fmt

--- src/V1/WeatherOracle.sol
+++ src/V1/WeatherOracle.sol
@@ -130,14 +130,14 @@ contract WeatherOracle is Ownable {
         return block.timestamp > currentWeather.timestamp + STALE_DATA_THRESHOLD;
     }
 
-        /**
+    /**
      * @dev Get last update timestamp
      */
     function getLastUpdateTimestamp() external view returns (uint256) {
         return currentWeather.timestamp;
     }
 
-        /**
+    /**
      * @dev Get weather condition by index
      */
     function getWeatherCondition(uint256 index) external view returns (string memory) {
